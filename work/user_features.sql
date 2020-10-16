daf1a7b9c47e71d2f26080257bbe691dc1134179
trtd56 <5toda6@gmail.com>
2020-10-16 13:07:09 +0900

    Update and rename features.sql to user_features.sql

---
+++ work/user_features.sql
@@ -0,0 +1,160 @@
+WITH add_lag_feat_table AS (
+SELECT
+  *,
+  timestamp - LAG (timestamp) OVER (ORDER BY user_id, timestamp, row_id) AS timediff
+FROM Kaggle_Riiid.train
+ORDER BY user_id, timestamp, row_id
+)
+
+SELECT
+  user_id,
+  0 AS cv_fold,
+  -- answer features
+  COUNT(answered_correctly) AS answered_correctly_count,
+  SUM(answered_correctly) AS answered_correctly_sum,
+  AVG(answered_correctly) AS answered_correctly_avg,
+  STDDEV(answered_correctly)　AS answered_correctly_std,
+  -- time features
+  MAX(timestamp) as timestamp_max,
+  -- timediff features
+  MAX(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_max,
+  MIN(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_min,
+  AVG(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_avg,
+  STDDEV(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_std,
+  -- prior_question_elapsed_time features
+  MAX(prior_question_elapsed_time) AS prior_question_elapsed_time_max,
+  MIN(prior_question_elapsed_time) AS prior_question_elapsed_time_min,
+  AVG(prior_question_elapsed_time) AS prior_question_elapsed_time_avg,
+  STDDEV(prior_question_elapsed_time) AS prior_question_elapsed_time_std,
+  -- prior_question_had_explanation features
+  SUM(CASE WHEN prior_question_had_explanation THEN 1 END) AS prior_question_had_explanation_sum,
+  AVG(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_avg,
+  STDDEV(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_std
+FROM Kaggle_Riiid.cv_fold_info_20201015
+INNER JOIN  add_lag_feat_table USING (row_id)
+WHERE cv_fold_0 != -1 AND cv_fold_0 != 0
+GROUP BY user_id
+
+UNION ALL
+
+SELECT
+  user_id,
+  1 AS cv_fold,
+  -- answer features
+  COUNT(answered_correctly) AS answered_correctly_count,
+  SUM(answered_correctly) AS answered_correctly_sum,
+  AVG(answered_correctly) AS answered_correctly_avg,
+  STDDEV(answered_correctly)　AS answered_correctly_std,
+  -- time features
+  MAX(timestamp) as timestamp_max,
+  -- timediff features
+  MAX(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_max,
+  MIN(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_min,
+  AVG(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_avg,
+  STDDEV(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_std,
+  -- prior_question_elapsed_time features
+  MAX(prior_question_elapsed_time) AS prior_question_elapsed_time_max,
+  MIN(prior_question_elapsed_time) AS prior_question_elapsed_time_min,
+  AVG(prior_question_elapsed_time) AS prior_question_elapsed_time_avg,
+  STDDEV(prior_question_elapsed_time) AS prior_question_elapsed_time_std,
+  -- prior_question_had_explanation features
+  SUM(CASE WHEN prior_question_had_explanation THEN 1 END) AS prior_question_had_explanation_sum,
+  AVG(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_avg,
+  STDDEV(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_std
+FROM Kaggle_Riiid.cv_fold_info_20201015
+INNER JOIN  add_lag_feat_table USING (row_id)
+WHERE cv_fold_0 != -1 AND cv_fold_0 != 1
+GROUP BY user_id
+
+UNION ALL
+
+SELECT
+  user_id,
+  2 AS cv_fold,
+  -- answer features
+  COUNT(answered_correctly) AS answered_correctly_count,
+  SUM(answered_correctly) AS answered_correctly_sum,
+  AVG(answered_correctly) AS answered_correctly_avg,
+  STDDEV(answered_correctly)　AS answered_correctly_std,
+  -- time features
+  MAX(timestamp) as timestamp_max,
+  -- timediff features
+  MAX(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_max,
+  MIN(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_min,
+  AVG(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_avg,
+  STDDEV(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_std,
+  -- prior_question_elapsed_time features
+  MAX(prior_question_elapsed_time) AS prior_question_elapsed_time_max,
+  MIN(prior_question_elapsed_time) AS prior_question_elapsed_time_min,
+  AVG(prior_question_elapsed_time) AS prior_question_elapsed_time_avg,
+  STDDEV(prior_question_elapsed_time) AS prior_question_elapsed_time_std,
+  -- prior_question_had_explanation features
+  SUM(CASE WHEN prior_question_had_explanation THEN 1 END) AS prior_question_had_explanation_sum,
+  AVG(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_avg,
+  STDDEV(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_std
+FROM Kaggle_Riiid.cv_fold_info_20201015
+INNER JOIN  add_lag_feat_table USING (row_id)
+WHERE cv_fold_0 != -1 AND cv_fold_0 != 2
+GROUP BY user_id
+
+UNION ALL
+
+SELECT
+  user_id,
+  3 AS cv_fold,
+  -- answer features
+  COUNT(answered_correctly) AS answered_correctly_count,
+  SUM(answered_correctly) AS answered_correctly_sum,
+  AVG(answered_correctly) AS answered_correctly_avg,
+  STDDEV(answered_correctly)　AS answered_correctly_std,
+  -- time features
+  MAX(timestamp) as timestamp_max,
+  -- timediff features
+  MAX(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_max,
+  MIN(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_min,
+  AVG(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_avg,
+  STDDEV(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_std,
+  -- prior_question_elapsed_time features
+  MAX(prior_question_elapsed_time) AS prior_question_elapsed_time_max,
+  MIN(prior_question_elapsed_time) AS prior_question_elapsed_time_min,
+  AVG(prior_question_elapsed_time) AS prior_question_elapsed_time_avg,
+  STDDEV(prior_question_elapsed_time) AS prior_question_elapsed_time_std,
+  -- prior_question_had_explanation features
+  SUM(CASE WHEN prior_question_had_explanation THEN 1 END) AS prior_question_had_explanation_sum,
+  AVG(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_avg,
+  STDDEV(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_std
+FROM Kaggle_Riiid.cv_fold_info_20201015
+INNER JOIN  add_lag_feat_table USING (row_id)
+WHERE cv_fold_0 != -1 AND cv_fold_0 != 3
+GROUP BY user_id
+
+UNION ALL
+
+SELECT
+  user_id,
+  4 AS cv_fold,
+  -- answer features
+  COUNT(answered_correctly) AS answered_correctly_count,
+  SUM(answered_correctly) AS answered_correctly_sum,
+  AVG(answered_correctly) AS answered_correctly_avg,
+  STDDEV(answered_correctly)　AS answered_correctly_std,
+  -- time features
+  MAX(timestamp) as timestamp_max,
+  -- timediff features
+  MAX(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_max,
+  MIN(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_min,
+  AVG(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_avg,
+  STDDEV(CASE WHEN timediff > 0 THEN timediff ELSE 0 END) AS timediff_std,
+  -- prior_question_elapsed_time features
+  MAX(prior_question_elapsed_time) AS prior_question_elapsed_time_max,
+  MIN(prior_question_elapsed_time) AS prior_question_elapsed_time_min,
+  AVG(prior_question_elapsed_time) AS prior_question_elapsed_time_avg,
+  STDDEV(prior_question_elapsed_time) AS prior_question_elapsed_time_std,
+  -- prior_question_had_explanation features
+  SUM(CASE WHEN prior_question_had_explanation THEN 1 END) AS prior_question_had_explanation_sum,
+  AVG(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_avg,
+  STDDEV(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_std
+FROM Kaggle_Riiid.cv_fold_info_20201015
+INNER JOIN  add_lag_feat_table USING (row_id)
+WHERE cv_fold_0 != -1 AND cv_fold_0 != 4
+GROUP BY user_id
