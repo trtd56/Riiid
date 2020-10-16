a964b136f6e1368bb1542206b5fe2ea24ffa5234
trtd56 <5toda6@gmail.com>
2020-10-16 14:20:56 +0900

    Update question_features.sql

--- work/question_features.sql
+++ work/question_features.sql
@@ -4,7 +4,8 @@ DECLARE fold INT64 DEFAULT 0;
 WITH add_lag_feat_table AS (
 SELECT
   *,
-  LAG (prior_question_elapsed_time) OVER (ORDER BY user_id, timestamp, row_id) AS question_elapsed_time
+  LEAD (prior_question_elapsed_time) OVER (ORDER BY user_id, timestamp, row_id) AS question_elapsed_time,
+  LEAD (prior_question_had_explanation) OVER (ORDER BY user_id, timestamp, row_id) AS question_had_explanation
 FROM Kaggle_Riiid.train
 ORDER BY user_id, timestamp, row_id
 )
@@ -12,6 +13,9 @@ ORDER BY user_id, timestamp, row_id
 SELECT
   content_id,
   fold AS cv_fold,
+  -- user features
+  COUNT(user_id) AS n_user,
+  COUNT(DISTINCT user_id) AS n_unique_user,
   -- answer features
   COUNT(answered_correctly) AS answered_correctly_count,
   SUM(answered_correctly) AS answered_correctly_sum,
@@ -23,9 +27,9 @@ SELECT
   AVG(question_elapsed_time) AS question_elapsed_time_avg,
   STDDEV(question_elapsed_time) AS question_elapsed_time_std,
   -- prior_question_had_explanation features
-  SUM(CASE WHEN prior_question_had_explanation THEN 1 END) AS prior_question_had_explanation_sum,
-  AVG(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_avg,
-  STDDEV(CASE WHEN prior_question_had_explanation THEN 1 ELSE 0 END) AS prior_question_had_explanation_std
+  SUM(CASE WHEN question_had_explanation THEN 1 END) AS question_had_explanation_sum,
+  AVG(CASE WHEN question_had_explanation THEN 1 ELSE 0 END) AS question_had_explanation_avg,
+  STDDEV(CASE WHEN question_had_explanation THEN 1 ELSE 0 END) AS question_had_explanation_std
 FROM Kaggle_Riiid.cv_fold_info_20201015
 INNER JOIN  add_lag_feat_table USING (row_id)
 WHERE cv_fold_0 != -1 AND cv_fold_0 != 0
