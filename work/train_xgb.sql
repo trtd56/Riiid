239ea22bbe6326e49dc5c94c8306797f4a8a82f9
trtd56 <5toda6@gmail.com>
2020-10-20 10:38:49 +0900

    Update train_xgb.sql

--- work/train_xgb.sql
+++ work/train_xgb.sql
@@ -1,19 +1,28 @@
-CREATE MODEL Kaggle_Riiid.xgb_v1
+CREATE MODEL Kaggle_Riiid.xgb_v1_5_fold0
 OPTIONS(MODEL_TYPE='BOOSTED_TREE_CLASSIFIER',
         BOOSTER_TYPE = 'GBTREE',
         NUM_PARALLEL_TREE = 1,
-        MAX_ITERATIONS = 50,
+        MAX_ITERATIONS = 1000,  -- early stoppingがあるのでなるだけ多く
         TREE_METHOD = 'HIST',
-        EARLY_STOP = FALSE,
-        SUBSAMPLE = 0.85,
-        INPUT_LABEL_COLS = ['answered_correctly'])
-AS SELECT
+        EARLY_STOP = True,
+        MIN_REL_PROGRESS=0.0001,
+        LEARN_RATE =0.1,
+        MAX_TREE_DEPTH=5, -- 5 ~ 8
+        COLSAMPLE_BYTREE=1.0,
+        COLSAMPLE_BYLEVEL=0.3, -- 0.5 ~0.1 を0.1刻み
+        SUBSAMPLE = 0.9,
+        MIN_TREE_CHILD_WEIGHT=1, -- 2^で増やしていく
+        -- 正則化はL1=0, L2=2(デフォルト)
+        INPUT_LABEL_COLS = ['answered_correctly'],
+        DATA_SPLIT_METHOD='CUSTOM',
+        DATA_SPLIT_COL='is_test')    
+AS SELECT 
   IFNULL(user_id, -1) AS user_id,
   IFNULL(timestamp, -1) AS timestamp,
   IFNULL(content_id, -1) AS content_id,
   IFNULL(task_container_id, -1) AS task_container_id,
   IFNULL(prior_question_elapsed_time, -1) AS prior_question_elapsed_time,
-  IFNULL(CAST(prior_question_had_explanation, INT64), -1) AS prior_question_had_explanation,
+  IFNULL(CAST(prior_question_had_explanation AS INT64), 0) AS prior_question_had_explanation,
   IFNULL(n_content, -1) AS n_content,
   IFNULL(n_unique_content, -1) AS n_unique_content,
   IFNULL(user_answered_correctly_sum, -1) AS user_answered_correctly_sum,
@@ -45,9 +54,10 @@ AS SELECT
   IFNULL(content_question_had_explanation_sum, -1) AS content_question_had_explanation_sum,
   IFNULL(content_question_had_explanation_avg, -1) AS content_question_had_explanation_avg,
   IFNULL(content_question_had_explanation_std, -1) AS content_question_had_explanation_std,
-  answered_correctly  -- target
+  answered_correctly,  -- target
+  cv_fold_0　= -1 as is_test,  -- test split rule
 FROM Kaggle_Riiid.cv_fold_info_20201015 
 INNER JOIN  Kaggle_Riiid.train USING (row_id)
 INNER JOIN Kaggle_Riiid.feat_u_v1 USING (user_id)
 INNER JOIN Kaggle_Riiid.feat_q_v1 ON feat_q_v1.question_id = train.content_id
-WHERE cv_fold_0 != -1 AND feat_u_v1.cv_fold = 0 AND feat_q_v1.cv_fold = 0
+WHERE feat_u_v1.cv_fold = 0 AND feat_q_v1.cv_fold = 0
