9aecf69371d1a54b32d34619640b4b223cd179e3
trtd56 <5toda6@gmail.com>
2020-10-21 13:01:00 +0900

    Update train_xgb.sql

--- work/train_xgb.sql
+++ work/train_xgb.sql
@@ -1,4 +1,4 @@
-CREATE MODEL Kaggle_Riiid.xgb_v2_fold0
+CREATE MODEL Kaggle_Riiid.xgb_v2_10_fold0
 OPTIONS(MODEL_TYPE='BOOSTED_TREE_CLASSIFIER',
         BOOSTER_TYPE = 'GBTREE',
         NUM_PARALLEL_TREE = 1,
@@ -7,21 +7,21 @@ OPTIONS(MODEL_TYPE='BOOSTED_TREE_CLASSIFIER',
         EARLY_STOP = True,
         MIN_REL_PROGRESS=0.0001,
         LEARN_RATE =0.1,
-        MAX_TREE_DEPTH=5, -- 5 ~ 8
+        MAX_TREE_DEPTH=8, -- 5 ~ 8
         COLSAMPLE_BYTREE=1.0,
-        COLSAMPLE_BYLEVEL=0.3, -- 0.5 ~0.1 を0.1刻み
+        COLSAMPLE_BYLEVEL=0.1, -- 0.5 ~0.1 を0.1刻み
         SUBSAMPLE = 0.9,
-        MIN_TREE_CHILD_WEIGHT=1, -- 2^で増やしていく
+        MIN_TREE_CHILD_WEIGHT=2, -- 2^で増やしていく
         -- 正則化はL1=0, L2=2(デフォルト)
         INPUT_LABEL_COLS = ['answered_correctly'],
         DATA_SPLIT_METHOD='CUSTOM',
         DATA_SPLIT_COL='is_test')    
 AS SELECT
   -- v1 features
-  CAST(user_id AS STRING) AS user_id,
+  -- CAST(user_id AS STRING) AS user_id,　膨大→embedする？
   IFNULL(timestamp, -1) AS timestamp,
-  CAST(content_id AS STRING) AS content_id,
-  CAST(task_container_id AS STRING) AS task_container_id,
+  -- CAST(content_id AS STRING) AS content_id, 膨大→embedする？
+  IFNULL(task_container_id, -1) AS task_container_id,
   IFNULL(prior_question_elapsed_time, -1) AS prior_question_elapsed_time,
   CAST(prior_question_had_explanation AS STRING) AS prior_question_had_explanation,
   IFNULL(n_content, -1) AS n_content,  
@@ -41,7 +41,7 @@ AS SELECT
   IFNULL(user_prior_question_had_explanation_sum, -1) AS user_prior_question_had_explanation_sum,
   IFNULL(user_prior_question_had_explanation_avg, -1) AS user_prior_question_had_explanation_avg,
   IFNULL(user_prior_question_had_explanation_std, -1) AS user_prior_question_had_explanation_std,
-  CAST(bundle_id AS STRING) AS bundle_id,
+  -- CAST(bundle_id AS STRING) AS bundle_id, 膨大→embedする？
   CAST(part AS STRING) AS part,
   IFNULL(n_user, -1) AS n_user,
   IFNULL(n_unique_user, -1) AS n_unique_user,
