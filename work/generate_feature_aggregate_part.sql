c5314dda6dbe0a29e90ccc5a76514c841c11250e
trtd56 <5toda6@gmail.com>
2020-10-23 13:16:29 +0900

    Update and rename part_features.sql to generate_feature_aggregate_part.sql

---
+++ work/generate_feature_aggregate_part.sql
@@ -0,0 +1,49 @@
+-- This query make aggregation features by part
+
+-- lag features
+WITH add_lag_feat_table AS (
+SELECT
+  *,
+  LEAD (prior_question_elapsed_time) OVER (ORDER BY user_id, timestamp, row_id) AS question_elapsed_time,
+  LEAD (prior_question_had_explanation) OVER (ORDER BY user_id, timestamp, row_id) AS question_had_explanation
+FROM Kaggle_Riiid.train
+ORDER BY user_id, timestamp, row_id
+)
+
+SELECT
+  -- ### key columns ###
+  part as part,
+  0 AS fold,
+  
+  -- ### target encoding features ###
+  -- answer features
+  COUNT(answered_correctly) AS part_answered_correctly_count,
+  SUM(answered_correctly) AS part_answered_correctly_sum,
+  AVG(answered_correctly) AS part_answered_correctly_avg,
+  STDDEV(answered_correctly)　AS part_answered_correctly_std,
+  
+  -- ### other aggregated contenst features ###
+  -- question features
+  COUNT(content_id) AS part_n_content_id,
+  -- user features
+  COUNT(DISTINCT user_id) AS part_n_unique_user,
+  -- task_container_id features
+  MAX(task_container_id) AS part_task_container_id_max,
+  MIN(task_container_id) AS part_task_container_id_min,
+  AVG(task_container_id) AS part_task_container_id_avg,
+  STDDEV(task_container_id) AS part_task_container_id_std,
+  -- prior_question_elapsed_time features
+  MAX(question_elapsed_time) AS part_question_elapsed_time_max,
+  MIN(question_elapsed_time) AS part_question_elapsed_time_min,
+  AVG(question_elapsed_time) AS part_question_elapsed_time_avg,
+  STDDEV(question_elapsed_time) AS part_question_elapsed_time_std,
+  -- prior_question_had_explanation features
+  SUM(CASE WHEN question_had_explanation THEN 0 ELSE 1 END) AS part_question_had_explanation_inv_sum,
+  AVG(CASE WHEN question_had_explanation THEN 0 ELSE 1 END) AS part_question_had_explanation_inv_avg,
+  STDDEV(CASE WHEN question_had_explanation THEN 0 ELSE 1 END) AS part_question_had_explanation_inv_std,
+  
+FROM Kaggle_Riiid.cv_fold_info_20201015
+INNER JOIN  add_lag_feat_table USING (row_id)
+INNER JOIN Kaggle_Riiid.questions ON question_id = content_id
+WHERE cv_fold_0 != -1
+GROUP BY part
